fb76574a7174b858bf77e61b4cf4539e63597397
mike mambwe <mikeapple056@gmail.com>
2023-03-09 23:53:15 +0200

    parallax

--- src/App.js
+++ src/App.js
@@ -6,7 +6,37 @@ function App() {
   const bg = "https://pixabay.com/images/id-3608029/"
   return (
     <div className="App">
-      <h3>Hello this is mike</h3>
+      <Parallax pages={4}>
+        <ParallaxLayer
+          style={{
+            background: "red",
+          }}
+        >
+          <h3>parallax try out</h3>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={1}
+          style={{
+            background: `url(${bg})`,
+          }}
+        >
+          <h3>Second text in this</h3>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={2}>
+          <h3>Second text in this</h3>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={3}
+          style={{
+            background: "orange",
+          }}
+        >
+          <h3>The end of this parallax</h3>
+        </ParallaxLayer>
+      </Parallax>
     </div>
   );
 }
